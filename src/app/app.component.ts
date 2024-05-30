8ed226c8b5693b29e36270fad9c59cce6d10f15d
Olexandra Korol <korol.olexandra@gmail.com>
2024-05-30 14:09:32 +0300

    set up and mainlogic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+
+import { DUMMY_USERS } from './dummy.users'
+import { TasksComponent } from './tasks/tasks.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'testAngular';
+  users = DUMMY_USERS
+  selectedUserId?: string
+
+  get userName() {
+    return this.users.find((user) => user.id == this.selectedUserId)
+  }
+
+  onGettingId(id: string) {
+    this.selectedUserId = id
+  }
 }
